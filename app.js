dcfc819e7493c1be3de6019ac7e1c5320fe40e2b
8braves_spenma <117264581+8braves-spenma@users.noreply.github.com>
2022-11-24 23:18:49 +0800

    Update app.js

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const seconds = document.getElementById('seconds');
 
 const currentYear = new Date().getFullYear();
 
-const newYearTime = new Date(`November 25 ${currentYear} 00:00:00`);
+const newYearTime = new Date(`November 29 ${currentYear} 00:00:00`);
 
 function uptadeCountdowntime(){
     const currentTime = new Date();
